c7f5052cf9de39e63a104ace7fa59355c5fca5cc
hailrakeqq <boklanura@gmail.com>
2022-07-28 10:58:19 +0300

    initial commit

---
+++ passwordGeneration.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+static const char symbols[] =
+    "0123456789"
+    "!@#$%%^&*"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz";
+int size = sizeof(symbols) - 1;
+
+int main(int argc, const char *argv[])
+{
+    printf("Enter password length: ");
+    int passwordLength = 8;
+    scanf("%d", &passwordLength);
+
+    srand(time(0));
+    for (int i = 1; i <= passwordLength; ++i)
+        printf("%c", symbols[rand() % size]);
+    printf("\n");
+    return 0;
+}
